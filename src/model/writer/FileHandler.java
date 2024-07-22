97d48b424a07d6c0d49b45aa042a105d3b2de8de
Wowan55555 <148986387+Wowan55555@users.noreply.github.com>
2024-07-22 09:55:51 +0300

    Проект работает

--- src/model/writer/FileHandler.java
+++ src/model/writer/FileHandler.java
@@ -3,12 +3,25 @@ package model.writer;
 import java.io.*;
 
 public class FileHandler implements Writer {
-    private String filePath = "";
+    private String filePath = "/path/to/your/file.txt";
+
+    public FileHandler() {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void save(Serializable serializable) {
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
             objectOutputStream.writeObject(serializable);
+            System.out.println("File saved to: " + filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -16,8 +29,12 @@ public class FileHandler implements Writer {
 
     @Override
     public Object read() {
+        System.out.println("Reading file from: " + filePath);
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath))) {
             return objectInputStream.readObject();
+        } catch (EOFException e) {
+            System.out.println("File is empty or corrupted.");
+            return null;
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             return null;
